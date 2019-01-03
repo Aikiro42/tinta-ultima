0ed80dd9c0172b704c00e029785b8b2c52f8ba2f
Aikiro42 <38589158+Aikiro42@users.noreply.github.com>
2019-01-04 07:58:27 +0800

    Update config-local.php
    tested github desktop change detection

--- config-local.php
+++ config-local.php
@@ -19,7 +19,7 @@ $dbname="ultima";
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 OR die ('Could not connect to MySQL: ' . mysqli_connect_error());
 
-
+//detect changes okay
 
 
 ?>
