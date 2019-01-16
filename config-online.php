fa6988eba11918f3e5330c5ce2ef8a74ec28c7b5
Aikiro42 <38589158+Aikiro42@users.noreply.github.com>
2019-01-16 19:16:47 +0800

    Push 20190116
    Added config-online.php
    Edited config.php
    - database conenction config: changed credentials
    Added article container box shadow
    Added header box shadow
    Started editing tinta about page

--- config-online.php
+++ config-online.php
@@ -2,21 +2,10 @@
 
 //initialize MySQL Connection Variables
 
-
-
-DEFINE('DB_USER','root');
-DEFINE('DB_PASSWORD','13-01104');
-DEFINE('DB_HOST', 'localhost');
-DEFINE('DB_NAME', 'ultima');
-
-/*
-
 DEFINE('DB_USER','b37bbc2c9689ea');
 DEFINE('DB_PASSWORD','43a055bf');
 DEFINE('DB_HOST', 'us-cdbr-gcp-east-01.cleardb.net	');
 DEFINE('DB_NAME', 'gcp_51dd865eb539f6526b03');
-
-*/
 
 //DEFINE('DB_HOST', '192.168.56.103');
 
@@ -29,6 +18,8 @@ $dbname="ultima";
 
 $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 OR die ('Could not connect to MySQL: ' . mysqli_connect_error());
+
+//detect changes okay
 
 
 ?>
